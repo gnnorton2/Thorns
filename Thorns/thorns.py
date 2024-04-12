182d2e3c209d3998418b9a644c96c52c35865c1c
gnnorton2 <161044764+gnnorton2@users.noreply.github.com>
2024-04-12 14:33:19 -0400

    Thorns1.1
    progress

--- Thorns/thorns.py
+++ Thorns/thorns.py
@@ -1,9 +1,55 @@
-import pygame, simpleGE
+
 """ thorns.py
     plant growing game
     Brielle"""
 
 import pygame, simpleGE
+
+#create labels for plant-growing categories, classes
+class WaterLabel(simpleGE.Label):
+    def __init__(self):
+        super().__init__()
+        self.text = "What will you water your plant with?"
+        self.center = (320, 50) #make this bigger
+        
+class ChooseWaterLabel(simpleGE.Label):
+    def __init__(self):
+        super().__init__()
+        self.text = "Water"
+        self.center = (145, 213)
+        
+class WateringCan(simpleGE.Sprite):
+    def __init__(self, scene):
+        super().__init__(scene)
+        self.setImage("wateringCan.png")
+        self.setSize(150, 150)
+        self.position = (145, 275)
+        
+class ChooseAcidLabel(simpleGE.Label):
+    def __init__(self):
+        super().__init__()
+        self.text = "Acid"
+        self.center = (495, 213)
+        
+class AcidVial(simpleGE.Sprite):
+    def __init__(self, scene):
+        super().__init__(scene)
+        self.setImage("acid.png")
+        self.setSize(150, 150)
+        self.position = (495, 300)
+        
+class ChooseNothingLabel(simpleGE.Label):
+    def __init__(self):
+        super().__init__()
+        self.text = "Nothing"
+        self.center = (320, 150)
+        
+class Nothing(simpleGE.Sprite):
+    def __init__(self, scene):
+        super().__init__(scene)
+        self.setImage("nothing.png")
+        self.setSize(150,150)
+        self.position = (320, 250)
 
 class Seed(simpleGE.Sprite):
     def __init__(self, scene):
@@ -11,6 +57,9 @@ class Seed(simpleGE.Sprite):
         self.setImage("seed.png") #add growing scre
         self.setSize(150, 150)
         self.position = (320, 240)
+        
+   # def process(self):
+     #   if self.
 
 class Game(simpleGE.Scene):
     def __init__(self):
@@ -18,8 +67,23 @@ class Game(simpleGE.Scene):
         self.setImage("grow.png")
        
         self.seed = Seed(self)
+        self.waterLabel = WaterLabel()
+        self.chooseWaterLabel = ChooseWaterLabel()
+        self.chooseAcidLabel = ChooseAcidLabel()
+        self.chooseNothingLabel = ChooseNothingLabel()
+        self.wateringCan = WateringCan(self)
+        self.acidVial = AcidVial(self)
+        self.nothing = Nothing(self)
         
-        self.sprites = [self.seed]
+        self.sprites = [self.seed,
+                        self.waterLabel,
+                        self.chooseWaterLabel,
+                        self.chooseAcidLabel,
+                        self.chooseNothingLabel,
+                        self.wateringCan,
+                        self.acidVial,
+                        self.nothing]
+        
         
         
 def main():
