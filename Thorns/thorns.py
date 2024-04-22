0bbb0d7068edfbae77b1c7bf7c55e25ce0f64a6f
gnnorton2 <161044764+gnnorton2@users.noreply.github.com>
2024-04-22 12:56:42 -0400

    Thorns 1.4

--- Thorns/thorns.py
+++ Thorns/thorns.py
@@ -5,27 +5,73 @@
 
 import pygame, simpleGE
 
+
+
+
+        
 class Sun(simpleGE.Sprite):
     def __init__(self, scene):
         super().__init__(scene)
-        self.setImage("sunshine.png")
+        self.setImage("invisible.png")
         self.setSize(150, 150)
         self.position = (145, 275)
+        self.moveSpeed = 0
+    
+    def reset(self):
+        self.setSize(150, 150)
+        self.position = (145, 275)
+        self.moveSpeed = 0
+        self.setImage("sunshine.png")
         
-        
+    def process(self):
+        if self.clicked:
+            self.moveSpeed = 5
+        if self.isKeyPressed(pygame.K_LEFT):
+            self.x -= self.moveSpeed
+        if self.isKeyPressed(pygame.K_RIGHT):
+            self.x += self.moveSpeed
+        if self.isKeyPressed(pygame.K_UP):
+            self.y -= self.moveSpeed
+        if self.isKeyPressed(pygame.K_DOWN):
+            self.y += self.moveSpeed
+    
 
 class Moon(simpleGE.Sprite):
     def __init__(self, scene):
         super().__init__(scene)
-        self.setImage("moon.png")
+        self.setImage("invisible.png")
+        self.setSize(150, 150)
+        self.position = (1, 1)
+        self.moveSpeed = 0
+    
+    def reset(self):
         self.setSize(150, 150)
         self.position = (495, 300)
-       
+        self.moveSpeed = 5
+        self.setImage("moon.png")
         
+    def process(self):
+        if self.clicked:
+            self.moveSpeed = 5
+        if self.isKeyPressed(pygame.K_LEFT):
+            self.x -= self.moveSpeed
+        if self.isKeyPressed(pygame.K_RIGHT):
+            self.x += self.moveSpeed
+        if self.isKeyPressed(pygame.K_UP):
+            self.y -= self.moveSpeed
+        if self.isKeyPressed(pygame.K_DOWN):
+            self.y += self.moveSpeed
+
+    
 class ChoiceOne(simpleGE.Sprite):
     def __init__(self, scene):
         super().__init__(scene)
         self.setImage("invisible.png")
+        self.setSize(150, 150)
+        self.position = (145, 275)
+        self.moveSpeed = 0
+    
+    def reset(self):
         self.setSize(150, 150)
         self.position = (145, 275)
         self.moveSpeed = 0
@@ -57,6 +103,12 @@ class ChoiceTwo(simpleGE.Sprite):
         self.setSize(150, 150)
         self.position = (495, 300)
         self.moveSpeed = 0
+        
+    def reset(self):
+        self.setSize(150, 150)
+        self.position = (495, 300)
+        self.moveSpeed = 0
+        
     def process(self):
          if self.clicked:
              self.moveSpeed = 5
@@ -74,7 +126,7 @@ class Nothing(simpleGE.Sprite):
     def __init__(self, scene):
         super().__init__(scene)
         self.setImage("invisible.png")
-        self.setSize(100,100)
+        self.setSize(50, 50)
         self.position = (550, 75)
 
 class Seed(simpleGE.Sprite):
@@ -108,36 +160,30 @@ class Game(simpleGE.Scene):
         self.setImage("grow.png")
        
         self.seed = Seed(self)
-        self.flowerPot = FlowerPot(self)
-        #self.waterLabel = WaterLabel()
-        #self.chooseWaterLabel = ChooseWaterLabel()
-       # self.chooseAcidLabel = ChooseAcidLabel()
-        #self.chooseNothingLabel = ChooseNothingLabel()
+        self.flowerPot = FlowerPot(self) 
         self.choiceOne = ChoiceOne(self)
         self.choiceTwo = ChoiceTwo(self)
         self.nothing = Nothing(self)
         self.sun = Sun(self)
         self.moon = Moon(self)
         
+        self.btnGrow = simpleGE.Button()
+        self.btnGrow.text = "Grow!"
+        self.btnGrow.center = (400, 240)
+        self.btnGrow.hide()
         
-      #  self.waterLabel = simpleGE.MultiLabel()
-      #  self.waterLabel.textLines = [
-      #      "Use the spacebar to drop the seed",
-       #     "then click on one option.",
-        #    " Use the arrow keys to move it",
-         #   "to the flower pot."
-          #  ]
-        #self.waterLabel.center = (320, 75)
-        #self.waterLabel.size = (350, 150)
+      
         
-        self.sprites = [self.nothing,
+        self.sprites = [self.flowerPot,
+                        self.sun,
+                        self.moon,
+                        self.nothing,
                         self.seed,
-                        self.flowerPot,
+                        self.btnGrow,
                         self.choiceOne,
                         self.choiceTwo,
-                        self.sun,
-                        self.moon
-                            #self.storm
+                        
+                           
                         
                         
                         
@@ -145,144 +191,112 @@ class Game(simpleGE.Scene):
                         ]
         
         self.plant = []
-        self.sun.hide()
-        self.moon.hide()
+        
     def process(self):
-       # plant = []
-        if self.seed.collidesWith(self.flowerPot):
+       
+        
+        if self.seed.collidesWith(self.flowerPot): 
+            self.seed.hide()
             
-            self.seed.position = (0, 0)
-            self.seed.setImage("invisible.png")
+            self.choiceOne.reset()
             self.choiceOne.setImage("wateringCan.png")
-            self.choiceOne.setSize(150, 150)
+            
+            self.choiceTwo.reset()
             self.choiceTwo.setImage("acid.png")
-            self.choiceTwo.setSize(150, 150)
+            
             self.nothing.setImage("nothing.png")
-            self.nothing.setSize(100, 100)
+         
     
         if self.choiceOne.collidesWith(self.flowerPot):
+            
             self.plant.append("3")
-            self.choiceOne.position = (145, 275)
+        
+            self.choiceOne.reset()
             self.choiceOne.setImage("gnomesoil.png")
-            self.choiceOne.moveSpeed = 0
+            
+            self.choiceTwo.reset()
             self.choiceTwo.setImage("rawmeat.png")
             print(f"{self.plant}")
-        if self.plant == ["3", "3"]:
-            self.choiceOne.hide()
-            self.choiceTwo.hide()
-            self.nothing.hide()
-            self.sun.show()
-            self.moon.show()
-        if self.plant == ["7", "3"]:
-            self.choiceOne.hide()
-            self.choiceTwo.hide()
-            self.nothing.hide()
-            self.sun.show()
-            self.moon.show()
-        if self.plant == ["1", "3"]:
-            self.choiceOne.hide()
-            self.choiceTwo.hide()
-            self.nothing.hide()
-            self.sun.show()
-            self.moon.show()
             
-        if self.choiceTwo.collidesWith(self.flowerPot):
+        
+        elif self.choiceTwo.collidesWith(self.flowerPot):
+            
             self.plant.append("7")
+            
+            self.choiceOne.reset()
             self.choiceOne.setImage("gnomesoil.png")
-            self.choiceTwo.moveSpeed = 0
+            
+            self.nothing.hide()
+                
+            self.choiceTwo.reset()
             self.choiceTwo.setImage("rawmeat.png")
-            self.choiceTwo.position = (495, 275)
-        if self.plant == ["7", "7"]:
-            self.choiceOne.hide()
-            self.choiceTwo.hide()
-            self.nothing.hide()
-            self.sun.show()
-            self.moon.show()
-        if self.plant == ["3", "7"]:
-            self.choiceOne.hide()
-            self.choiceTwo.hide()
-            self.nothing.hide()
-            self.sun.show()
-            self.moon.show()
-        if self.plant == ["1", "7"]:
-            self.choiceOne.hide()
-            self.choiceTwo.hide()
-            self.nothing.hide()
-            self.sun.show()
-            self.moon.show()
-        
+            
             print(f"{self.plant}")
-        if self.nothing.clicked:
+            
+            
+        elif self.nothing.clicked:
             self.plant.append("1")
-            self.nothing.position = (550, 75)
+            
+            self.choiceOne.reset()
             self.choiceOne.setImage("gnomesoil.png")
-            self.nothing.moveSpeed = 0
-            self.choiceTwo.setImage("rawmeat.png")
-        if self.plant == ["1", "1"]:
-            self.choiceOne.hide()
-            self.choiceTwo.hide()
-            self.nothing.hide()
-            self.sun.show()
-            self.moon.show()
-        if self.plant == ["3", "1"]:
-            self.choiceOne.hide()
-            self.choiceTwo.hide()
-            self.nothing.hide()
-            self.sun.show()
-            self.moon.show()
-        if self.plant == ["7", "1"]:
-            self.choiceOne.hide()
-            self.choiceTwo.hide()
-            self.nothing.hide()
-            self.sun.show()
-            
-            self.moon.show()
-            
-        #if self.sun.clicked:
-            
-            
-     
-        
-          
-            
-
-        #newimage over sprite OR hide and put new images over top
-
-#class NextGame(simpleGE.Scene):
- #   def __init__(self):
-  #      super().__init__()   
-  #      self.setImage("grow.png")
-        
-   #     self.flowerPot = FlowerPot(self)
-        
-   #     self.sprites = [self.flowerPot,
-                        
-                        
                     
-                      #  ]
+            self.choiceTwo.reset()
+            self.choiceTwo.setImage("rawmeat.png")
             
+            print(f"{self.plant}")
             
-    
+        elif self.plant == ["1", "1"] or self.plant == ["1", "3"] or self.plant == ["1", "7"] or self.plant == ["3", "1"] or self.plant == ["3", "3"] or self.plant == ["3", "7"] or self.plant == ["7", "1"] or self.plant == ["7", "3"] or self.plant == ["7", "7"]:
+            self.choiceOne.hide()
+            self.choiceTwo.hide()
+            self.nothing.hide()
+            self.sun.reset()
+            self.moon.reset()
+            
+            if self.sun.clicked:
+                self.plant.append("1")
+                print(f"{self.plant}")
+                self.sun.hide()
+                self.moon.hide()
+                self.btnGrow.show()
+            elif self.moon.clicked:
+                self.plant.append("7")
+                print(f"{self.plant}")
+                self.moon.hide()
+                self.sun.hide()
+                self.btnGrow.show()
+                
+        if self.btnGrow.clicked:
+            print("grow clicked")
+            self.stop()
+                
+        
+class GetPlant(simpleGE.Scene):
+    def __init__(self):
+        super().__init__()
+        self.setImage("introScreenDraft.png")
+        
+        self.rename = simpleGE.MultiLabel()
+        self.rename.textLines = [
+        "Congrats on your plant!",  
+        "Would you like to rename this plant?"
+            ]
+        
+        self.rename.center = (320, 100)
+        self.rename.size = (400, 150)
+        self.sprites = [self.rename]
+        
+        
+        
         
         
         
 def main():
-   # keepGoing = True
-   # Game.start()
-   # while keepGoing:
-     game =Game()
-     game.start()
+  
+    game = Game()
+    game.start()
         
+    getPlant = GetPlant()
+    getPlant.start()
         
-        #game.start()
-      #  print(instructions.response)
-    
-      #  if Game.response == "Next":    
-        #    nextGame = NextGame()
-       #     nextGame.start()
-            
-      #  else:
-        #    keepGoing = False
-  #
 if __name__ == "__main__":
     main()
